0595d1f41b32e2d7dbaccfa1920325a064667671
Herman Singh <kartstig@gmail.com>
2016-03-03 13:04:24 -0500

    Basic login/logout stuff

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,17 +1,61 @@
 #!/usr/bin/ python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, \
+    url_for, flash
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, login_user, \
+    current_user, logout_user
 from config import app_config
 
+from tracker.models.User import User
+
 # Main Application and Config
-app = Flask('cmm')
+app = Flask(__name__)
 app.config.from_object(app_config)
 
 # Database initialization
 db = SQLAlchemy(app=app)
 
+# Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(userid):
+    return get_user({'id': userid})
+
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello"
+    bindings = {
+        'error': None,
+        'user': current_user
+    }
+    return render_template('index.html', **bindings)
+
+@app.route('/login/', methods=['GET','POST'])
+def login():
+    errors ={}
+    if request.method == 'POST':
+        user = get_user({'username': request.form['username']})
+        if user:
+            if user.valid_password(request.form['password']):
+                login_user(user, remember=True)
+                flash("Logged in successfully")
+                return redirect(url_for('index'))
+            else:
+                errors["password"] = ["Invalid Password"]
+        else:
+            errors["username"] = ["Invalid Username"]
+    return render_template('login.html', errors=errors)
+
+@app.route("/logout/", methods=["GET", "POST"])
+def logout():
+    logout_user()
+    flash("Logged out successfully.")
+    return redirect(url_for("login"))
+
+def get_user(filter):
+    try:
+        return db.session.query(User).filter_by(**filter).one()
+    except:
+        return None
